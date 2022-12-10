23543a318f380e7f1b1c37edbbfc22de61d7b488
Alexandre <alexmcs@gmail.com>
2022-12-10 11:42:24 -0300

    Melhorando estilo

--- src/App.js
+++ src/App.js
@@ -5,29 +5,29 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className='output'>
-        <div className='calculator-title'></div>
+        <div className='calculator-title'> React Calculator</div>
         <div className='previous-operand'>123 *</div>
         <div className='current-operand'>1234</div>
       </div>
-      <button>AC</button>
-      <button>DEL</button>
-      <button>%</button>
-      <button>/</button>
+      <button className='ac-del-btn'>AC</button>
+      <button className='ac-del-btn'>DEL</button>
+      <button className='operand-btn'>%</button>
+      <button className='operand-btn'>/</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
-      <button>X</button>
+      <button className='operand-btn'>X</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
-      <button>-</button>
+      <button className='operand-btn'>-</button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button>+</button>
-      <button>.</button>
+      <button className='operand-btn'>+</button>
+      <button className='operand-btn'>.</button>
       <button>0</button>
-      <button className='span-two'>=</button>
+      <button className='span-two operand-btn'>=</button>
     </div>
   );
 }
