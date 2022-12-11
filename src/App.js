4cacca88cd6e805849ad1649d1e9bfd36623465d
Alexandre <alexmcs@gmail.com>
2022-12-11 19:52:10 -0300

    Criando componentes da calculadora

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
+import DigitBtn from './DigitBtn';
+import OperationBtn from './OperationBtn';
 import './App.css';
 
+export const actions = {
+  add_digit: "add-digit",
+  choose_operation: "choose-operation",
+  clear: "clear",
+  delete_digit: "delete-digit",
+  evaluate: "evaluate",
+}
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case actions.add_digit:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      }
+    default:
+      break;
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  )
+
+  dispatch({ type: actions.add_digit, payload: { digit: 1 }})
+
   return (
     <div className="calculator-grid">
       <div className='output'>
         <div className='calculator-title'> React Calculator</div>
-        <div className='previous-operand'>123 *</div>
-        <div className='current-operand'>1234</div>
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
       </div>
       <button className='ac-del-btn'>AC</button>
       <button className='ac-del-btn'>DEL</button>
       <button className='operand-btn'>%</button>
-      <button className='operand-btn'>/</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button className='operand-btn'>X</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button className='operand-btn'>-</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button className='operand-btn'>+</button>
-      <button className='operand-btn'>.</button>
-      <button>0</button>
+      <OperationBtn operation="/" dispatch={dispatch} />
+      <DigitBtn digit="7" dispatch={dispatch} />
+      <DigitBtn digit="8" dispatch={dispatch} />
+      <DigitBtn digit="9" dispatch={dispatch} />
+      <OperationBtn operation="X" dispatch={dispatch} />
+      <DigitBtn digit="4" dispatch={dispatch} />
+      <DigitBtn digit="5" dispatch={dispatch} />
+      <DigitBtn digit="6" dispatch={dispatch} />
+      <OperationBtn operation="-" dispatch={dispatch} />
+      <DigitBtn digit="1" dispatch={dispatch} />
+      <DigitBtn digit="2" dispatch={dispatch} />
+      <DigitBtn digit="3" dispatch={dispatch} />
+      <OperationBtn operation="+" dispatch={dispatch} />
+      <DigitBtn digit="." dispatch={dispatch} />
+      <DigitBtn digit="0" dispatch={dispatch} />
       <button className='span-two operand-btn'>=</button>
     </div>
   );
